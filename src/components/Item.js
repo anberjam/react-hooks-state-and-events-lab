d95a3b446d7fd216a1cc60f3c2fca63d843b42b9
anberjamz <anberjam@yahoo.com>
2021-01-11 23:20:54 -0500

    Done.

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [location, setLocation] = useState(false)
+
+  function handleLocation () {
+    setLocation((location) => !location)
+  }
+  
   return (
-    <li className="">
+    <li className={location ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={location ? "remove" : "add"} onClick = {handleLocation}>{location? "Remove From" : "Add to"} Cart</button>
     </li>
   );
 }
